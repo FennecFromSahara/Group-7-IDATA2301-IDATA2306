3285be1aa7ecd5a9b5a68f7fd215797d8545c965
shv-gitman <98781052+shv-gitman@users.noreply.github.com>
2023-02-15 01:28:06 +0100

    added spring initializer

---
+++ coffeshop-backend/src/test/java/no/ntnu/group7/coffeshop/CoffeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.group7.coffeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
