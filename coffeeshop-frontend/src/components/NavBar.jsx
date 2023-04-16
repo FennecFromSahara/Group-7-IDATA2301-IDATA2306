692400a20f72ab0e69d1c3e9f04eee3ec4356c4d
shv-gitman <98781052+shv-gitman@users.noreply.github.com>
2023-04-16 06:25:43 +0200

    Added menu to NavBar

--- coffeeshop-frontend/src/components/NavBar.jsx
+++ coffeeshop-frontend/src/components/NavBar.jsx
@@ -4,19 +4,101 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import { Box, IconButton, Typography } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import LocalCafeIcon from "@mui/icons-material/LocalCafe";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
+import Paper from "@mui/material/Paper";
+import Grow from "@mui/material/Grow";
+import Popper from "@mui/material/Popper";
 
 const pages = ["Home", "Products", "About Us"];
 const link = ["/", "/products", "/about"];
 
+/**
+ * A responsive nav bar that includes a menu button for smaller resolutions.
+ *
+ * @returns {JSX.Element} The JSX code for a nav bar for the coffeeshop
+ */
 function NavBar() {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
   return (
     <AppBar position="sticky" elevation={0} sx={{ height: "8vh" }}>
       <Toolbar>
-        <Link to="/">
-          <LocalCafeIcon sx={{ mr: 1, mt: 1 }} />
-        </Link>
+        <Button
+          ref={anchorRef}
+          id="composition-button"
+          aria-controls={open ? "composition-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+        >
+          <MenuIcon />
+        </Button>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom-start" ? "left top" : "left bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    {pages.map((page, index) => (
+                      <MenuItem>
+                        <Link
+                          to={link[index]}
+                          style={{ textDecoration: "none" }}
+                        >
+                          {page}
+                        </Link>
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
         <Typography variant="h2" href="/">
           MOCHA NOKA KAFE
         </Typography>
