902628542034577f741689e96fa16339aa07021b
adrianrdntnu <98705814+adrianrdntnu@users.noreply.github.com>
2023-05-25 20:10:51 +0200

    Review now displays users shoppingcart items

--- coffeeshop-frontend/src/pages/Checkout/Review.jsx
+++ coffeeshop-frontend/src/pages/Checkout/Review.jsx
@@ -1,33 +1,39 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Grid from '@mui/material/Grid';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Grid from "@mui/material/Grid";
+import { useState, useEffect } from "react";
+import {
+  getShoppingCart,
+  getProductById,
+  getShoppingCartTotal,
+} from "../../hooks/apiService";
 
-const products = [
-  {
-    name: 'Product 1',
-    desc: 'A nice thing',
-    price: '$9.99',
-  },
-  {
-    name: 'Product 2',
-    desc: 'Another thing',
-    price: '$3.45',
-  },
-  {
-    name: 'Product 3',
-    desc: 'Something else',
-    price: '$6.51',
-  },
-  {
-    name: 'Product 4',
-    desc: 'Best thing of all',
-    price: '$14.11',
-  },
-  { name: 'Shipping', desc: '', price: 'Free' },
-];
+// const products = [
+//   {
+//     name: 'Product 1',
+//     desc: 'A nice thing',
+//     price: '$9.99',
+//   },
+//   {
+//     name: 'Product 2',
+//     desc: 'Another thing',
+//     price: '$3.45',
+//   },
+//   {
+//     name: 'Product 3',
+//     desc: 'Something else',
+//     price: '$6.51',
+//   },
+//   {
+//     name: 'Product 4',
+//     desc: 'Best thing of all',
+//     price: '$14.11',
+//   },
+//   { name: 'Shipping', desc: '', price: 'Free' },
+// ];
 
 // const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
 // const payments = [
@@ -37,22 +43,51 @@ const products = [
 //   { name: 'Expiry date', detail: '04/2024' },
 // ];
 
-export default function Review({addressInfo, paymentInfo}) {
+export default function Review({ addressInfo, paymentInfo }) {
+  const [shoppingCart, setShoppingCart] = useState([]);
+  const [total, setTotal] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const shoppingCartData = await getShoppingCart();
+        setShoppingCart(shoppingCartData);
+
+        const fetchedProducts = [];
+
+        for (const item of shoppingCartData) {
+          const product = await getProductById(item.productId);
+          fetchedProducts.push(product);
+        }
+
+        setProducts(fetchedProducts);
+
+        const totalData = await getShoppingCartTotal();
+        setTotal(totalData);
+      } catch (err) {
+        setError(`Error fetching data: ${err.message}`);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const addresses = [
-    addressInfo.address1, 
-    addressInfo.address2, 
-    addressInfo.city, 
-    addressInfo.state, 
-    addressInfo.zip, 
-    addressInfo.country
+    addressInfo.address1,
+    addressInfo.address2,
+    addressInfo.city,
+    addressInfo.state,
+    addressInfo.zip,
+    addressInfo.country,
   ];
 
   const payments = [
-    { name: 'Card type', detail: 'Visa' },
-    { name: 'Card holder', detail: paymentInfo.cardName },
-    { name: 'Card number', detail: paymentInfo.cardNumber },
-    { name: 'Expiry date', detail: paymentInfo.expDate },
+    { name: "Card type", detail: "Visa" },
+    { name: "Card holder", detail: paymentInfo.cardName },
+    { name: "Card number", detail: paymentInfo.cardNumber },
+    { name: "Expiry date", detail: paymentInfo.expDate },
   ];
 
   return (
@@ -61,17 +96,30 @@ export default function Review({addressInfo, paymentInfo}) {
         Order summary
       </Typography>
       <List disablePadding>
-        {products.map((product) => (
-          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-            <ListItemText primary={product.name} secondary={product.desc} />
-            <Typography variant="body2">{product.price}</Typography>
-          </ListItem>
-        ))}
+        {products.map((product) => {
+          // Find the item in the shopping cart that matches the current product's ID
+          const cartItem = shoppingCart.find(
+            (item) => item.productId === product.id
+          );
+
+          // Get the quantity from the cart item, defaulting to 0 if not found
+          const quantity = cartItem ? cartItem.quantity : 0;
+
+          return (
+            <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+              <ListItemText
+                primary={product.name}
+                secondary={`Quantity: ${quantity}`}
+              />
+              <Typography variant="body2">{product.price} kr</Typography>
+            </ListItem>
+          );
+        })}
 
         <ListItem sx={{ py: 1, px: 0 }}>
           <ListItemText primary="Total" />
           <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-            $34.06
+            {total} kr
           </Typography>
         </ListItem>
       </List>
@@ -81,7 +129,7 @@ export default function Review({addressInfo, paymentInfo}) {
             Shipping
           </Typography>
           <Typography gutterBottom>John Smith</Typography>
-          <Typography gutterBottom>{addresses.join(', ')}</Typography>
+          <Typography gutterBottom>{addresses.join(", ")}</Typography>
         </Grid>
         <Grid item container direction="column" xs={12} sm={6}>
           <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
